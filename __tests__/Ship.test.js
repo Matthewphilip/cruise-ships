e5b80e294c262eb467ead4a1f07df038d22d7cd7
Matthew Wiggins <mattwiggins5@gmail.com>
2022-04-21 11:41:14 +0100

    addition of dock method

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,23 +1,43 @@
 /* globals describe it expect */
 const Ship = require('../src/Ship.js');
+const Port = require('../src/Port.js');
 
 describe('Ship constructor', () => {
     it('returns an object', () => {
         expect(new Ship()).toBeInstanceOf(Object);
     });
 
-    it('has a current port', () => {
-        const ship = new Ship('Dover');
-        expect(ship.currentPort).toBe('Dover');
+    it('has a starting port', () => {
+        // const ship = new Ship('Dover');
+        const port = new Port('Dover');
+        const ship = new Ship(port);
+
+
+        // expect(ship.currentPort).toBe('Dover');
+        expect(ship.currentPort).toBe(port);
     })
 });
 
 describe('set sail', () => {
     it('can set sail', () => {
-        const ship = new Ship('Dover');
+        // const ship = new Ship('Dover');
+        const port = new Port('Dover');
+        const ship = new Ship(port);
 
         ship.setSail();
 
         expect(ship.currentPort).toBeFalsy();
+    })
+})
+
+describe('dock', () => {
+    it('can dock at a different port', () => {
+        const dover = new Port('Dover');
+        const ship = new Ship(dover);
+
+        const calais = new Port('Calais');
+        ship.dock(calais);
+
+        expect(ship.currentPort).toBe(calais);
     })
 })
--- src/Ship.js
+++ src/Ship.js
@@ -1,10 +1,16 @@
+const Port = require("./Port");
+
 function Ship () {
-    this.currentPort = 'Dover';
+    this.currentPort = "Dover";
 } 
 
 Ship.prototype.setSail = function () {
     this.currentPort = false;
 }
+
+Ship.prototype.dock = function () {
+    this.currentPort = port;
+    };
 
 
 module.exports = Ship;
